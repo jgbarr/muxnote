cce9d6878df41bf9f679e2907deccba563b7a821
jgbarr <jeff@jeffbarr.com>
2012-02-11 19:34:36 -0800

    first commit

---
+++ Gemfile
@@ -0,0 +1,17 @@
+source :rubygems
+gem 'sinatra'
+gem 'data_mapper'
+gem 'dm-core'
+gem 'dm-migrations'
+gem 'rack-flash'
+gem 'omniauth', :git => 'git://github.com/intridea/omniauth.git'
+gem 'oa-oauth'
+
+group :development do
+  gem 'dm-sqlite-adapter'
+end
+
+group :production do
+  gem 'pg'
+  gem 'dm-postgres-adapter'
+end
---
+++ app.rb
@@ -0,0 +1,168 @@
+%w(rubygems oa-oauth dm-core dm-sqlite-adapter dm-migrations sinatra).each { |dependency| require dependency }
+require 'sinatra'
+require 'dm-core'
+require 'dm-migrations'
+
+DataMapper.setup(:default, ENV['DATABASE_URL'] || "sqlite3://#{Dir.pwd}/database.db")
+
+class User
+  include DataMapper::Resource
+  property :id,         Serial
+  property :uid,        String
+  property :name,   String
+  property :nickname,       String
+  property :created_at, DateTime
+  has n, :notes
+end
+
+class Note
+  include DataMapper::Resource
+    property :id, Serial
+    property :user_id, String
+    property :content, Text, :required => true
+    property :complete, Boolean, :required => true, :default => false
+    property :created_at, DateTime
+    property :updated_at, DateTime
+    belongs_to :user, :required => false
+end
+
+DataMapper.finalize
+DataMapper.auto_upgrade!
+APP_TITLE = "Mocknote"
+
+use OmniAuth::Strategies::Twitter, 'l0kAScjfkrGN970LCRcwg', 'F5B5BZQCxcJGFts0OPAVmBCwQJYwUOpBJjT12oT5CA'
+
+enable :sessions
+
+helpers do
+  def current_user
+    @current_user ||= User.get(session[:user_id]) if session[:user_id]
+  end
+end
+
+get '/' do
+  if current_user
+    # The following line just tests to see that it's working.
+    #   If you've logged in your first user, '/' should load: "1 ... 1";
+    #   You can then remove the following line, start using view templates, etc.
+    #current_user.id.to_s + " ... " + session[:user_id].to_s
+    redirect '/all'
+  else
+    '<a href="/sign_up">create an account</a> or <a href="/sign_in">sign in with Twitter</a>'
+    # if you replace the above line with the following line, 
+    #   the user gets signed in automatically. Could be useful. 
+    #   Could also break user expectations.
+    # redirect '/auth/twitter'
+  end
+end
+
+get '/auth/:name/callback' do
+  auth = request.env['omniauth.auth']
+puts "AUTH #{auth}"
+puts "AUTH #{auth['uid']}"
+puts "AUTH #{auth['user_info']['name']}"
+puts "AUTH #{auth['user_info']['nickname']}"
+#current_user.find_or_create_by_provider_and_uid(auth['provider'], auth['uid'])
+ user = User.first_or_create({ :uid => auth["uid"]}, {
+  :uid => auth["uid"],
+  :name => auth["user_info"]["name"],
+  :nickname => auth["user_info"]["nickname"],
+  :created_at => Time.now })
+  session[:user_id] = user.id
+  redirect '/all'
+end
+
+# aany of the following routes should work to sign the user in: 
+#   /sign_up, /signup, /sign_in, /signin, /log_in, /login
+["/sign_in/?", "/signin/?", "/log_in/?", "/login/?", "/sign_up/?", "/signup/?"].each do |path|
+  get path do
+    redirect '/auth/twitter'
+  end
+end
+
+# either /log_out, /logout, /sign_out, or /signout will end the session and log the user out
+["/sign_out/?", "/signout/?", "/log_out/?", "/logout/?"].each do |path|
+  get path do
+    session[:user_id] = nil
+    redirect '/'
+  end
+end
+
+
+#get '/' do
+#  @notes = MockNote.all :order => :id.desc
+#  @title = APP_TITLE
+#  erb :home
+#end
+
+post '/' do
+  n = Note.new
+  n.content = params[:content]
+  n.created_at = Time.now
+  n.updated_at = Time.now
+  puts "USER WAS :: #{params[:user]}"
+  n.user_id = params[:user]
+  n.save
+  redirect '/all'
+end
+
+put '/:id' do
+  n = Note.get params[:id]
+  n.content = params[:content]
+  n.complete = params[:complete] ? 1 : 0
+  n.updated_at = Time.now
+  n.save
+  redirect '/all'
+end
+
+get '/books' do
+  @notes = Note.all( :content.like => '%book%')
+  @title = 'Books you want to read.'
+  erb :books
+end
+
+get '/movies' do
+  @notes = Note.all( :content.like => '%movie%')
+  @title = 'Movies you want to see.'
+  erb :movies
+end
+
+get '/all' do
+  @notes = Note.all :order => :id.desc  
+  @title = "All notes"
+  erb :all
+end
+
+delete '/:id/' do
+  n = Note.get params[:id]
+  n.destory
+  redirect '/'
+end
+
+get '/:id/edit' do
+  @note = Note.get params[:id]
+  @title = "Categorize ##{params[:id]}" 
+  erb :edit
+end
+
+get '/:id/complete' do
+  n = Note.get params[:id]
+  #n.complete = n.complete ? 0 : 1
+  #n.updated_at = Time.now
+  n.destroy
+  redirect '/'
+end
+
+get '/:id/complete' do
+ n = Note.get params[:id]
+ n.destroy
+ redirect '/'
+end
+
+get '/about' do
+  'Muxnote is a simplified mobile app to track movies you want to see and books you want to read.'
+end
+
+not_found do  
+  halt 404, 'page not found'  
+end 
