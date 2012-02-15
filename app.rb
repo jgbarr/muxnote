0b2a51b0daa50ff098ddaf3ad1c043853e784112
jgbarr <jeff@jeffbarr.com>
2012-02-15 02:04:45 -0800

    change require code in app

--- app.rb
+++ app.rb
@@ -1,9 +1,6 @@
 require 'sinatra'
 require 'dm-core'
 require 'dm-migrations'
-require 'omniauth-oauth'
-require 'omniauth'
-require 'omniauth/oauth'
 
 DataMapper.setup(:default, ENV['DATABASE_URL'] || "sqlite3://#{Dir.pwd}/data2.db")
 
