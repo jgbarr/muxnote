6d8ee48d2b848c57ed51fae162cc10349db41d31
jgbarr <jeff@jeffbarr.com>
2012-02-15 01:56:03 -0800

    fixing Gemfiles

--- app.rb
+++ app.rb
@@ -2,7 +2,7 @@
 require 'sinatra'
 require 'dm-core'
 require 'dm-migrations'
-
+require 'omniauth-oauth'
 DataMapper.setup(:default, ENV['DATABASE_URL'] || "sqlite3://#{Dir.pwd}/data2.db")
 
 class User
