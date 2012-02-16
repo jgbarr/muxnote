037379f32e333c32a3ee73a564b1a33645a82645
jgbarr <jeff@jeffbarr.com>
2012-02-16 13:03:04 -0800

    debugging on heroku

--- app.rb
+++ app.rb
@@ -1,8 +1,8 @@
-%w(rubygems dm-core dm-migrations sinatra).each { |dependency| require dependency }
+%w(rubygems oa-oauth dm-core dm-migrations sinatra).each { |dependency| require dependency }
 require 'sinatra'
 require 'dm-core'
 require 'dm-migrations'
-require 'omniauth/oauth'
+#require 'omniauth/oauth'
 #require 'omniauth'
 #require 'omniauth-twitter'
 #gem 'twitter'
