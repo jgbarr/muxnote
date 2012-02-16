e70261f146b050ce7cf56187d73720613abd6066
jgbarr <jeff@jeffbarr.com>
2012-02-16 15:15:13 -0800

    changes for heroku

--- app.rb
+++ app.rb
@@ -48,8 +48,8 @@ get '/auth/:name/callback' do
   
   user = User.first_or_create({ :uid => auth["uid"]}, {
   :uid => auth["uid"],
-  :name => auth["user_info"]["name"],
-  :nickname => auth["user_info"]["nickname"],
+  :name => auth["user"]["name"],
+  :nickname => auth["user"]["nickname"],
   :created_at => Time.now })
   puts "USER WAS:: #{user}"
   session[:user_id] = user.id
