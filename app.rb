7e0d46a3f868141b4620495bffc4edaa1bbc570c
jgbarr <jeff@jeffbarr.com>
2012-02-16 16:22:42 -0800

    changes for heroku

--- app.rb
+++ app.rb
@@ -3,7 +3,7 @@ require 'sinatra'
 require 'dm-core'
 require 'dm-migrations'
 require 'omniauth-oauth'
-require 'omniauth'
+require 'omniauth-twitter'
 
 DataMapper.setup(:default, ENV['DATABASE_URL'] || "sqlite3://#{Dir.pwd}/data2.db")
 
